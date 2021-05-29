6d14ebb548d0ecc1628b3f552487633ffb3954e2
tkmktkhr <mukku.wordpress@gmail.com>
2021-05-29 20:10:15 +0900

    feat: add fastify with express

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,28 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-app.listen(3030);
+import fastify from "fastify";
+import fastifyExpress from "fastify-express";
+import express from "express";
+// import { router } from '@/src/infrastructures/routers'
+const server = fastify();
+export const router = express.Router();
+router.get('/ping', (req, res) => {
+    console.log('get ping');
+    const jsonObj = {
+        a: 'test',
+        n: null
+    };
+    res.send(jsonObj);
+});
+async function build() {
+    await server.register(fastifyExpress).after(() => { server.use(router); });
+    return server;
+}
+;
+build()
+    .then(server => server.listen(3030, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+}))
+    .catch(console.log);
