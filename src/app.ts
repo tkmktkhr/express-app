6e10644dcfdaba6ddddc7d36df97b0e160d58f11
tkmktkhr <mukku.wordpress@gmail.com>
2021-05-29 20:51:23 +0900

    chore: install eslint prettier

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,43 @@
-import fastify from "fastify";
-import fastifyExpress from "fastify-express";
-import express from "express";
+import fastify from 'fastify';
+import fastifyExpress from 'fastify-express';
+import express from 'express';
 // import { router } from '@/src/infrastructures/routers'
+import log4js from 'log4js';
+
+const logger = log4js.getLogger();
+logger.level = 'debug';
+logger.debug('Some debug messages');
 
 const server = fastify();
 
-
-export const router = express.Router();
+const router = express.Router();
 
 router.get('/ping', (req, res) => {
-  console.log('get ping');
+  logger.debug('get ping');
   const jsonObj = {
     a: 'test',
-    n: null
-  }
-  res.send(jsonObj)
+    n: null,
+  };
+  res.send(jsonObj);
 });
 
-async function build () {
-  await server.register(fastifyExpress).after(() => {server.use(router)});
+async function build() {
+  await server.register(fastifyExpress).after(() => {
+    server.use(router);
+  });
   return server;
-};
+}
 
 build()
-.then(server => server.listen(3030, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-}))
-.catch(console.log);
-
-
+  .then((server) =>
+    server.listen(3030, (err, address) => {
+      if (err) {
+        logger.debug(err);
+        /*global process*/
+        /*eslint no-undef: "error"*/
+        process.exit(1);
+      }
+      logger.debug(`Server listening at ${address}`);
+    }),
+  )
+  .catch(logger.debug);
